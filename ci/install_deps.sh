3708faac345eb2c05c5cf85a7dff7a9671b09599
Florian Merz <flomerz@gmail.com>
2019-03-28 17:17:03 +0100

    enable codebuild - 6

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -6,11 +6,11 @@ export RUST_VERSION=1.33.0
 set -eux
 dpkgArch="$(dpkg --print-architecture)"
 case "${dpkgArch##*-}" in
-    amd64) rustArch='x86_64-unknown-linux-gnu'; rustupSha256='2d4ddf4e53915a23dda722608ed24e5c3f29ea1688da55aa4e98765fc6223f71' ;
-    armhf) rustArch='armv7-unknown-linux-gnueabihf'; rustupSha256='be85f50dc70ee239c5bb6acb60080797841a1e7c45fbf6bae15d6bd4b37ce0e5' ;
-    arm64) rustArch='aarch64-unknown-linux-gnu'; rustupSha256='454f00a86be75ab070149bac1f541a7b39e5d3383d6da96ad2b929867ed40167' ;
-    i386) rustArch='i686-unknown-linux-gnu'; rustupSha256='179e3b39f11037a708874e750081f7c0d3e1a6a4c431c2ecee2295acc7b696af' ;
-    *) echo >&2 "unsupported architecture: ${dpkgArch}"; exit 1 ;
+    amd64) rustArch='x86_64-unknown-linux-gnu'; rustupSha256='2d4ddf4e53915a23dda722608ed24e5c3f29ea1688da55aa4e98765fc6223f71' ;;
+    armhf) rustArch='armv7-unknown-linux-gnueabihf'; rustupSha256='be85f50dc70ee239c5bb6acb60080797841a1e7c45fbf6bae15d6bd4b37ce0e5' ;;
+    arm64) rustArch='aarch64-unknown-linux-gnu'; rustupSha256='454f00a86be75ab070149bac1f541a7b39e5d3383d6da96ad2b929867ed40167' ;;
+    i386) rustArch='i686-unknown-linux-gnu'; rustupSha256='179e3b39f11037a708874e750081f7c0d3e1a6a4c431c2ecee2295acc7b696af' ;;
+    *) echo >&2 "unsupported architecture: ${dpkgArch}"; exit 1 ;;
 esac
 url="https://static.rust-lang.org/rustup/archive/1.16.0/${rustArch}/rustup-init"
 wget "$url"
