e3b12216c5dca0de5792752dbe6b34dbbbf00069
Florian Merz <flomerz@gmail.com>
2019-06-11 11:26:48 +0200

    fix picture upload

--- src/graphql_api/app.rs
+++ src/graphql_api/app.rs
@@ -79,6 +79,10 @@ pub fn graphql_app<T: AsyncCisClientTrait + Clone + Send + Sync + 'static>(
         .data(GraphQlState {
             schema: Arc::new(schema),
         })
-        .service(web::resource("").route(web::post().to(graphql::<T>)))
+        .data(web::JsonConfig::default().limit(1_048_576))
+        .service(
+            web::resource("")
+                .route(web::post().to(graphql::<T>)),
+        )
         .service(web::resource("/graphiql").route(web::get().to(graphiql)))
 }
