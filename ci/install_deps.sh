5f23450badd62bbdc06cf8eda1aa1817446f42df
Rob Tucker <rtucker@mozilla.com>
2021-05-18 11:59:51 -0400

    Setting build version to v3.5.4

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -2,7 +2,7 @@ export RUSTUP_HOME=/usr/local/rustup
 export CARGO_HOME=/usr/local/cargo
 export PATH=/usr/local/cargo/bin:$PATH
 export RUST_VERSION=1.46.0
-export DESIRED_VERSION=3.5.4
+export DESIRED_VERSION="v3.5.4"
 
 set -eux
 dpkgArch="$(dpkg --print-architecture)"
